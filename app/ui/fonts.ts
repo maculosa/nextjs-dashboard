3ffeda2d851b798c41fc9b113cfb61416cd0d9ab
Qingfeng Liu <maculosa@163.com>
2025-02-04 00:39:43 +0800

    fix: kk

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -3,5 +3,6 @@ import { Inter, Lusitana } from 'next/font/google'
 export const inter = Inter({ subsets: ['latin'] })
 
 export const lusitana = Lusitana({
-    weight: "700"
+    weight: "700",
+    subsets: ['latin']
 })
